3293cf85d987288dd8832a12d5d14307026c1ce7
Dhruvil1802 <dhruvilpatel180201@gmail.com>
2025-04-23 21:11:42 -0400

    Update HomePage.js

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -43,7 +43,7 @@ const HomePage = () => {
   return (
     <div className="homepage"    
     style={{
-      backgroundImage: `url("${host}/Media/banner/31.png")`,
+      backgroundImage: "https://ecomapi-production-f9d8.up.railway.app/Media/banner/31.png",
     }}>
       <Header />
       <SideMenu />
