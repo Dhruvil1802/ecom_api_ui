e4850ed8fc01422c6cb9e41a2aeb8a6dbd0a5a7d
Dhruvil1802 <dhruvilpatel180201@gmail.com>
2025-05-01 02:01:01 -0400

    changed banner url

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -43,7 +43,7 @@ const HomePage = ({navigate}) => {
   return (
     <div className="homepage"    
     style={{
-      backgroundImage: `url("${host}/staticfiles/banner/31.png")`,
+      backgroundImage: `url("${host}/static/banner/31.png")`,
     }}>
       <Header navigate={navigate} />
       <SideMenu />
