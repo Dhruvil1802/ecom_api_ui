9ac9748807a098486ae8beec741e8cf65dea5ee7
Dhruvil1802 <dhruvilpatel180201@gmail.com>
2025-08-10 03:39:38 -0400

    edited urls

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -69,7 +69,7 @@ function Login({showHomePage, setToken}) {
         <div className="login_page">
             <div className="sub_login_page">
                 <div className='img-div'>
-                <img src={`${local}/Media/login_image/loginpage.jpeg`} className="background_image" />
+                <img src={`${host}/Media/login_image/loginpage.jpeg`} className="background_image" />
                 </div>
                 <div className="form_div">
                     <div className="logo_div">
